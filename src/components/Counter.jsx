35c3ca6243938e833912bb7a5f293380771f9afc
Sumanth66946 <sumanthkumar66946@gmail.com>
2025-08-12 09:26:17 +0530

    Initial commit - Counter Webpage

---
+++ src/components/Counter.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import './Counter.css'
+class Counter extends Component{
+    state= {count:0}
+    onIncreament=()=>{
+        this.setState((prevState)=>({count:prevState.count+1}))
+    }
+    onDecreament=()=>{
+        this.setState((prevState)=>({count:prevState.count-1}))
+    }
+    render(){
+        const {count}=this.state
+        return (
+        <div className="container">
+            <h1 className="heading">Counter</h1>
+            <p className="count">Count {count}</p>
+            <div>
+                <button className="button" onClick={this.onIncreament}>
+                    Increase
+                </button>
+                <button className="button" onClick={this.onDecreament}>
+                    Decrease
+                </button>
+            </div>
+        </div>
+        );
+    }
+}
+export default Counter
